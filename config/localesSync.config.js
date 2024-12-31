e54d08af0847700a2a71277c06909c96c25424e5
Kuroiteiken <ntavsan35@gmail.com>
2024-12-31 12:48:36 +0300

    Turkish translations

--- config/localesSync.config.js
+++ config/localesSync.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   primaryLanguage: 'en',
-  secondaryLanguages: ['de', 'es', 'fr', 'it', 'ja', 'ko', 'zh-Hans', 'zh-Hant', 'ru','tr-TR'],
+  secondaryLanguages: ['de', 'es', 'fr', 'it', 'ja', 'ko', 'zh-Hans', 'zh-Hant', 'ru','tr'],
   localesFolder: './locales',
   spaces: 2,
   useEmptyString: true,
